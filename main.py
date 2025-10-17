4925f567fa04e2d22423fff22f83eb43a719f62b
Jesarim <jesalbertorm@ciencias.unam.mx>
2025-10-16 19:26:03 -0600

    Filling n/a rows with the mean of each column

--- main.py
+++ main.py
@@ -12,8 +12,27 @@ def read_csv_file(path_file: str):
       return data
    
 
+def create_list_data(frame, column_name):
+   list_data = [data for data in frame[column_name]]
+   return list_data
+
+def display_info_df(frame):
+   print(frame.info())
+
+
+   
+   
+
 if __name__=='__main__':
    path_f = "sports_data_missing.csv"
    data = read_csv_file(path_f)
    print(data.head())
-   print(data.info())
+   display_info_df(data)
+
+   data = data.fillna(data.mean(numeric_only=True))
+   data = data.round(1)
+
+   ##Inspect the cleaned data
+   display_info_df(data)
+   missing_values = data.isnull().sum()
+   print(f'Missing values on each column:\n {missing_values}')
